ed16c811bfa0ccf9150161d1e45fbec4a36b5559
Jack Sheridan <jackrileysheridan@gmail.com>
2023-03-12 16:56:52 -0700

    popups

--- modal.js
+++ modal.js
@@ -2,9 +2,9 @@
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
-img.onclick = function(){
+var images = document.getElementsByTagName('img');
+function onClickImg() {
 	modal.style.display = "block";
 	modalImg.src = this.src;
 	const scrollY = window.scrollY
@@ -12,14 +12,41 @@ img.onclick = function(){
 	document.body.style.top = `-${scrollY}px`;
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-	const scrollY = document.body.style.top;
-document.body.style.position = '';
-document.body.style.top = '';
-window.scrollTo({top: parseInt(scrollY || '0') * -1, left: 0, behavior: "instant"});
+for(const image of images) {
+	if (!image.classList.contains("imgModal-content")) {
+		image.onclick = onClickImg
+	}
 }
+
+
+function closeModal() {
+	modal.style.display = "none";
+	const scrollY = document.body.style.top;
+	document.body.style.position = '';
+	document.body.style.top = '';
+	window.scrollTo({top: parseInt(scrollY || '0') * -1, left: 0, behavior: "instant"});
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("close");
+// When the user clicks on <span> (x), close the modal
+span.onclick = closeModal
+
+// When the user clicks outside of the img, close the modal
+modal.onclick = function(evt) {
+	if (event.target.closest(".imgModal-content")) return
+	closeModal()
+}
+
+document.onkeydown = function(evt) {
+	evt = evt || window.event;
+	var isEscape = false;
+	if ("key" in evt) {
+	isEscape = (evt.key === "Escape" || evt.key === "Esc");
+	} else {
+	isEscape = (evt.keyCode === 27);
+	}
+	if (isEscape) {
+	    closeModal()
+	}
+};
